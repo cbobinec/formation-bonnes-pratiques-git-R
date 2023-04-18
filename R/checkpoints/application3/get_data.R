1f080e5ae5781adcdffb234ff4736b38e7f13344
Perrine Bauer <perrine.bauer@insee.fr>
2023-04-18 14:38:24 +0200

    donne accès à la table de travail stockee sous v:/formations-stat/formation bonnes pratiques

--- R/checkpoints/application3/get_data.R
+++ R/checkpoints/application3/get_data.R
@@ -1,10 +1,4 @@
 
-df <- 
-  aws.s3::s3read_using(
-    FUN = readr::read_csv2,
-    object = "diffusion/bonnes-pratiques-r/rp_2016_individu_sample.csv",
-    bucket = "projet-formation",
-    opts = list("region" = "")
-  )
+df <- readr::read_csv2("V:/Formations-Stats/2023 Formation bonnes pratiques Git et R - DR44/Bases/rp_2016_individu_sample.csv")
 
 arrow::write_parquet(df, "individu_reg.parquet")
